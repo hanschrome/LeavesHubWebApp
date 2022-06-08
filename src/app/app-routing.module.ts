a1ef7786ea338a69acf85a5aead3eac8f03290c5
Hans Castro <hanscastroadmn@gmail.com>
2022-06-08 07:09:15 +0200

    Moved reset component to its directory

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AccessComponent} from "./user/public/access/access.component";
 import {LostPasswordComponent} from "./user/public/lost-password/lost-password.component";
+import {ResetPasswordComponent} from "./user/public/reset-password/reset-password.component";
 
 const routes: Routes = [
   {path: '', component: AccessComponent},
-  {path: 'lost-password', component: LostPasswordComponent}
+  {path: 'lost-password', component: LostPasswordComponent},
+  {path: 'reset-password', component: ResetPasswordComponent},
 ];
 
 @NgModule({

a518651369d3d6ce67d0e4ca3688e5f89fb9586a
Hans Castro <hanscastroadmn@gmail.com>
2022-06-08 07:03:51 +0200

    Modified view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AccessComponent} from "./user/public/access/access.component";
+import {LostPasswordComponent} from "./user/public/lost-password/lost-password.component";
 
 const routes: Routes = [
-  {path: '', component: AccessComponent}
+  {path: '', component: AccessComponent},
+  {path: 'lost-password', component: LostPasswordComponent}
 ];
 
 @NgModule({

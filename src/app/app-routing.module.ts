60e69250e223fe3b8c404ad6174618d1e4ccf8b7
Hans Castro <hanscastroadmn@gmail.com>
2022-06-08 07:21:33 +0200

    [Layout] Set password after email verification

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import {AccessComponent} from "./user/public/access/access.component";
 import {LostPasswordComponent} from "./user/public/lost-password/lost-password.component";
 import {ResetPasswordComponent} from "./user/public/reset-password/reset-password.component";
+import {EmailVerifiedComponent} from "./user/public/email-verified/email-verified.component";
 
 const routes: Routes = [
   {path: '', component: AccessComponent},
   {path: 'lost-password', component: LostPasswordComponent},
   {path: 'reset-password', component: ResetPasswordComponent},
+  {path: 'email-verified', component: EmailVerifiedComponent},
 ];
 
 @NgModule({

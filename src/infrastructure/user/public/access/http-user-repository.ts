7c3dd3b5ca020a216098c8f7162d95caec4dd7bc
Hans Castro <hanscastroadmn@gmail.com>
2022-06-09 16:06:39 +0000

    [User] Repository create user by email

--- src/infrastructure/user/public/access/http-user-repository.ts
+++ src/infrastructure/user/public/access/http-user-repository.ts
@@ -1,6 +1,10 @@
 import {IUserRepository} from '../../../../domain/user/contracts/user-repository/i-user-repository';
 import {HttpClient} from "@angular/common/http";
 import {map, Observable} from "rxjs";
+import {
+  IUserCreationHttpResponse
+} from "../../../../domain/user/contracts/user-repository/responses/i-user-creation-http-response";
+import {UserCreationHttpResponse} from "./user-creation-http-response";
 
 export class HttpUserRepository implements IUserRepository {
 

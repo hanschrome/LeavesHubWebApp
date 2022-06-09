7ce499e49897b560f06d93171d8557651c6410ce
Hans Castro <hanscastroadmn@gmail.com>
2022-06-09 16:18:24 +0000

    [User] User creation http request

--- src/infrastructure/user/public/access/http-user-repository.ts
+++ src/infrastructure/user/public/access/http-user-repository.ts
@@ -5,19 +5,23 @@ import {
   IUserCreationHttpResponse
 } from "../../../../domain/user/contracts/user-repository/responses/i-user-creation-http-response";
 import {UserCreationHttpResponse} from "./user-creation-http-response";
+import {environment} from "../../../../environments/environment";
 
 export class HttpUserRepository implements IUserRepository {
 
-  constructor(private http: HttpClient) {
-  }
+  private _url = '/v1/register'
+
+  constructor(private http: HttpClient) {}
 
   createUserByEmail(email: string): Observable<IUserCreationHttpResponse> {
-    return this.http.post('', {}).pipe(
-      map((rawHttpResponse) => {
-        const httpResponse = new UserCreationHttpResponse();
-
-        return httpResponse;
+     return this.http.post(environment.api + this.url, {email: email}).pipe(
+      map((rawHttpResponse: any) => {
+        return new UserCreationHttpResponse(rawHttpResponse.error, rawHttpResponse.isSuccess);
       })
     );
+  }
+
+  get url(): string {
+    return this._url
   }
 }
--- src/infrastructure/user/public/access/user-creation-http-response.ts
+++ src/infrastructure/user/public/access/user-creation-http-response.ts
@@ -3,6 +3,15 @@ import {
 } from "../../../../domain/user/contracts/user-repository/responses/i-user-creation-http-response";
 
 export class UserCreationHttpResponse implements IUserCreationHttpResponse {
+
+  private _error_code: string;
+  private _is_success: boolean;
+
+  constructor(error_code: string, is_success: boolean) {
+    this._error_code = error_code;
+    this._is_success = is_success;
+  }
+
   get errorCode(): string {
     return "";
   }

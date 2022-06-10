84317d8a2af5ed907957f3886d680ca0c49def6b
Hans Castro <hanscastroadmn@gmail.com>
2022-06-10 18:22:30 +0000

    [Formaŧting] Reformat app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AccessComponent } from './user/public/access/access.component';
-import {FormsModule} from "@angular/forms";
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AccessComponent} from './user/public/access/access.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
 import {MatCardModule} from "@angular/material/card";
 import {MatToolbarModule} from "@angular/material/toolbar";
-import { LostPasswordComponent } from './user/public/lost-password/lost-password.component';
-import { ResetPasswordComponent } from './user/public/reset-password/reset-password.component';
-import { EmailVerifiedComponent } from './user/public/email-verified/email-verified.component';
-import { ProfileComponent } from './user/private/profile/profile.component';
-import { ForestListComponent } from './forest/components/forest-list/forest-list.component';
-import { ForestComponent } from './forest/forest.component';
-import { TreeComponent } from './forest/tree/tree.component';
-import { BranchComponent } from './forest/tree/branch/branch.component';
-import { LeafComponent } from './forest/tree/branch/leaf/leaf.component';
+import {LostPasswordComponent} from './user/public/lost-password/lost-password.component';
+import {ResetPasswordComponent} from './user/public/reset-password/reset-password.component';
+import {EmailVerifiedComponent} from './user/public/email-verified/email-verified.component';
+import {ProfileComponent} from './user/private/profile/profile.component';
+import {ForestListComponent} from './forest/components/forest-list/forest-list.component';
+import {ForestComponent} from './forest/forest.component';
+import {TreeComponent} from './forest/tree/tree.component';
+import {BranchComponent} from './forest/tree/branch/branch.component';
+import {LeafComponent} from './forest/tree/branch/leaf/leaf.component';
 import {MatListModule} from "@angular/material/list";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {HttpUserRepository} from "../infrastructure/user/public/access/http-user-repository";
+import {HttpClient, HttpClientModule, HttpHeaders} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -36,19 +39,25 @@ import {MatListModule} from "@angular/material/list";
     BranchComponent,
     LeafComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        FormsModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatButtonModule,
-        MatCardModule,
-        MatToolbarModule,
-        MatListModule
-    ],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatListModule,
+    MatProgressSpinnerModule,
+    HttpClientModule
+  ],
+  providers: [
+    HttpUserRepository,
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}

33a8ea43e4539c2b2e1d891050eccbcb1b723507
Hans Castro <hanscastroadmn@gmail.com>
2022-06-13 21:11:19 +0000

    [Users] Service integration

--- src/app/user/public/lost-password/lost-password.service.ts
+++ src/app/user/public/lost-password/lost-password.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {HttpUserRepository} from "../../../../infrastructure/user/public/access/http-user-repository";
+import {
+  IUserRecoverPasswordRequestHttpResponse
+} from "../../../../domain/user/contracts/user-repository/responses/i-user-recover-password-request-http-response";
+import {Observable} from "rxjs";
+import {
+  IUserResetPasswordHttpResponse
+} from "../../../../domain/user/contracts/user-repository/responses/i-user-reset-password-http-response";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LostPasswordService {
 
-  constructor() { }
+  constructor(private httpUserRepository: HttpUserRepository) { }
+
+  recoverUserPasswordByEmail(email: string): Observable<IUserRecoverPasswordRequestHttpResponse> {
+    return this.httpUserRepository.recoverUserPasswordByEmail(email);
+  }
+
+  resetUserPasswordByToken(token: string): Observable<IUserResetPasswordHttpResponse> {
+    return this.httpUserRepository.resetUserPasswordByToken(token);
+  }
 }

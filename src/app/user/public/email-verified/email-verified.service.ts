2276ddd9b8dee08b2f39c555945c2c6f1b94b167
Hans Castro <hanscastroadmn@gmail.com>
2022-06-13 20:51:50 +0000

    [Users] Added to the service the method for verify user by email.

--- src/app/user/public/email-verified/email-verified.service.ts
+++ src/app/user/public/email-verified/email-verified.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {HttpUserRepository} from "../../../../infrastructure/user/public/access/http-user-repository";
+import {Observable} from "rxjs";
+import {
+  IUserEmailVerifyHttpResponse
+} from "../../../../domain/user/contracts/user-repository/responses/i-user-email-verify-http-response";
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailVerifiedService {
 
-  constructor() { }
+  constructor(private httpUserRepository: HttpUserRepository) { }
+
+  verifyEmail(email: string, token: string): Observable<IUserEmailVerifyHttpResponse> {
+    return this.httpUserRepository.verifyUserByEmail(email, token);
+  }
 }

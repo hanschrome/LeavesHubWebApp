79707705fd61c1608bbfe3f9379c6537c9383eda
Hans Castro <hanscastroadmn@gmail.com>
2022-06-13 20:55:26 +0000

    [Users] Added recover password by email http contracts

--- src/domain/user/contracts/user-repository/i-user-repository.ts
+++ src/domain/user/contracts/user-repository/i-user-repository.ts
@@ -2,6 +2,7 @@ import {Observable} from "rxjs";
 import {IUserCreationHttpResponse} from "./responses/i-user-creation-http-response";
 import {IUserLoginHttpResponse} from "./responses/i-user-login-http-response";
 import {IUserEmailVerifyHttpResponse} from "./responses/i-user-email-verify-http-response";
+import {IUserRecoverPasswordRequestHttpResponse} from "./responses/i-user-recover-password-request-http-response";
 
 export interface IUserRepository {
   createUserByEmail(email: string): Observable<IUserCreationHttpResponse>;
@@ -9,4 +10,6 @@ export interface IUserRepository {
   loginUserByEmailAndPassword(email: string, password: string): Observable<IUserLoginHttpResponse>;
 
   verifyUserByEmail(email: string, token: string): Observable<IUserEmailVerifyHttpResponse>;
+
+  recoverUserPasswordByEmail(email: string): Observable<IUserRecoverPasswordRequestHttpResponse>;
 }
---
+++ src/domain/user/contracts/user-repository/responses/i-user-recover-password-request-http-response.ts
@@ -0,0 +1,4 @@
+export interface IUserRecoverPasswordRequestHttpResponse {
+  get error(): string|null;
+  get isSuccess(): boolean;
+}

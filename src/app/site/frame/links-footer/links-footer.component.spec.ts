a0162df3fbe01692f1b3d436e16039b567850b1c
Hans Castro <hanscastroadmn@gmail.com>
2022-06-14 18:01:31 +0200

    [Site] Frame footer links component creation

---
+++ src/app/site/frame/links-footer/links-footer.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LinksFooterComponent } from './links-footer.component';
+
+describe('LinksFooterComponent', () => {
+  let component: LinksFooterComponent;
+  let fixture: ComponentFixture<LinksFooterComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LinksFooterComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LinksFooterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

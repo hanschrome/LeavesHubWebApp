028e6a4d5c3cd4c36ab8a6891c689b359f05feb3
Hans Castro <hanscastroadmn@gmail.com>
2022-06-15 05:46:11 +0000

    [Users] Added logic to communicate with the service in lost password component

--- src/app/user/public/lost-password/lost-password.component.ts
+++ src/app/user/public/lost-password/lost-password.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from "@angular/forms";
+import {LostPasswordService} from "./lost-password.service";
 
 @Component({
   selector: 'app-lost-password',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LostPasswordComponent implements OnInit {
 
-  constructor() { }
+  status = 0;
+
+  STATUS_CLEAN = 0;
+  STATUS_SUCCESS = 1;
+  STATUS_ERROR = 2;
+
+  error = '';
+
+  lostPasswordFormControl = new FormControl('', [Validators.required]);
+
+  constructor(private lostPasswordService: LostPasswordService) { }
 
   ngOnInit(): void {
   }
 
+  onSubmitResetPasswordFormEventHandler(): void {
+    this.recoverUserPasswordByEmail(this.lostPasswordFormControl.value || '');
+  }
+
+  recoverUserPasswordByEmail(email: string): void {
+    this.status = this.STATUS_CLEAN;
+
+    this.lostPasswordService.recoverUserPasswordByEmail(email).subscribe(
+      (response) => {
+        if (!response.isSuccess) {
+          this.error = response.error || '';
+          this.status = this.STATUS_ERROR;
+          return;
+        }
+
+        this.status = this.STATUS_SUCCESS;
+      }
+    )
+  }
 }

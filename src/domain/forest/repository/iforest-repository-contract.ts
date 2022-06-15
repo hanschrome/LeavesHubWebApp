392ad4c8bca45d65bcba1e69dd301ddd268100ad
Hans Castro <hanscastroadmn@gmail.com>
2022-06-15 17:19:14 +0000

    [DOMAIN][Forest] Added contracts in forest contract repository

--- src/domain/forest/repository/iforest-repository-contract.ts
+++ src/domain/forest/repository/iforest-repository-contract.ts
@@ -1,3 +1,8 @@
+import {Observable} from "rxjs";
+import {IUser} from "../../user/i-user";
+
 interface IForestRepositoryContract {
-  
+  getForestListByUser(user: IUser): Observable<IForestList>;
+
+  getForestByForestId(forestId: IForestId): Observable<IForest>;
 }

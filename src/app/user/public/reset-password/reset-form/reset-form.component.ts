b96bc1f8b11ad6ccac8ea6ef8918ecc14488d4e8
Hans Castro <hanscastroadmn@gmail.com>
2022-06-15 06:07:05 +0000

    [Users] Added inner subtitle on reset password

--- src/app/user/public/reset-password/reset-form/reset-form.component.ts
+++ src/app/user/public/reset-password/reset-form/reset-form.component.ts
@@ -12,6 +12,7 @@ import {ActivatedRoute} from "@angular/router";
 export class ResetFormComponent implements OnInit {
 
   @Input() tokenParameter = '';
+  @Input() subtitle = ''
 
   ERROR_MISMATCH = 'mismatch';
 

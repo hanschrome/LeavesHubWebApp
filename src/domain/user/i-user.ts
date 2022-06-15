255296af4fb7de56523d4d8e34b507f8e9a1f2f5
Hans Castro <hanscastroadmn@gmail.com>
2022-06-15 17:35:08 +0000

    [DOMAIN][User] Added password to interface and user implementation

--- src/domain/user/i-user.ts
+++ src/domain/user/i-user.ts
@@ -1,5 +1,11 @@
 import {IUserId} from "./properties/i-user-id";
+import {IUserEmail} from "./properties/i-user-email";
+import {IUserPassword} from "./properties/i-user-password";
 
 export interface IUser {
   get id(): IUserId|null;
+
+  get email(): IUserEmail|null;
+
+  get password(): IUserPassword|null;
 }
--- src/domain/user/user.ts
+++ src/domain/user/user.ts
@@ -1,14 +1,21 @@
 import {IUser} from "./i-user";
 import {IUserId} from "./properties/i-user-id";
 import {IUserEmail} from "./properties/i-user-email";
+import {IUserPassword} from "./properties/i-user-password";
 
 export class User implements IUser {
   private readonly _id: IUserId;
   private readonly _email: IUserEmail|null;
+  private readonly _password: IUserPassword|null;
 
-  constructor(id: IUserId, email: IUserEmail|null) {
+  constructor(
+    id: IUserId,
+    email: IUserEmail|null = null,
+    password: IUserPassword|null = null,
+  ) {
     this._id = id;
     this._email = email;
+    this._password = password;
   }
 
   get id(): IUserId {
@@ -17,5 +24,9 @@ export class User implements IUser {
 
   get email(): IUserEmail|null {
     return this._email;
+  }
+
+  get password(): IUserPassword|null {
+    return this._password;
   }
 }

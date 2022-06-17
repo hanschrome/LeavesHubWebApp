b3e7658a65e88d158e30bfd825b8683e1d6d7e53
Hans Castro <hanscastroadmn@gmail.com>
2022-06-17 16:28:22 +0000

    [Domain] Forest repository contract

--- src/domain/forest/repository/iforest-repository-contract.ts
+++ src/domain/forest/repository/iforest-repository-contract.ts
@@ -1,7 +1,9 @@
 import {Observable} from "rxjs";
 import {IUser} from "../../user/i-user";
+import {IForestList} from "../i-forest-list";
+import {IForest} from "../i-forest";
 
-interface IForestRepositoryContract {
+export interface IForestRepositoryContract {
   getForestListByUser(user: IUser): Observable<IForestList>;
 
   getForestByForestId(forestId: IForestId): Observable<IForest>;

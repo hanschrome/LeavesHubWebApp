9714c8c6ef4036ed3515cb733f6bbf252f15fae1
Hans Castro <hanscastroadmn@gmail.com>
2022-06-24 03:36:48 +0000

    [Infra] Added Local storage session repository

--- src/infrastructure/forest/rest-forest-repository.service.ts
+++ src/infrastructure/forest/rest-forest-repository.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class RestForestRepositoryService implements IForestRepositoryContract {
+export class RestForestRepositoryService { // implements IForestRepositoryContract
 
-  constructor() { }
 }
---
+++ src/infrastructure/user/private/local-storage-session-repository.ts
@@ -0,0 +1,5 @@
+import {ISessionRepository} from "../../../domain/user/session/contracts/session-repository/i-session-repository";
+
+export class LocalStorageSessionRepository implements ISessionRepository {
+
+}

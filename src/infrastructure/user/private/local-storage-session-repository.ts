e7d18d31bd82297b0696ff5e48d38b261375ee23
Hans Castro <hanscastroadmn@gmail.com>
2022-06-26 18:22:44 +0000

    [Domain] Added session repository implementation

--- src/infrastructure/user/private/local-storage-session-repository.ts
+++ src/infrastructure/user/private/local-storage-session-repository.ts
@@ -1,5 +1,14 @@
 import {ISessionRepository} from "../../../domain/user/session/contracts/session-repository/i-session-repository";
+import {ISession} from "../../../domain/user/session/i-session";
+import {Session} from "../../../domain/user/session/session";
 
 export class LocalStorageSessionRepository implements ISessionRepository {
+
+  findSession(): ISession {
+    return new Session('', 0, 0);
+  }
+
+  saveSession(session: ISession): void {
+  }
 
 }

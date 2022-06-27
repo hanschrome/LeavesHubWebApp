bcba48a10cae7446344ea2ed20becf55a7170208
Hans Castro <hanscastroadmn@gmail.com>
2022-06-27 21:05:54 +0000

    [App] Added user access save session and load session

--- src/app/user/public/access/user-access.service.ts
+++ src/app/user/public/access/user-access.service.ts
@@ -9,13 +9,19 @@ import {
 } from "../../../../domain/user/contracts/user-repository/responses/i-user-login-response";
 import {IUserEmail} from "../../../../domain/user/properties/i-user-email";
 import {IUserPassword} from "../../../../domain/user/properties/i-user-password";
+import {LocalStorageSessionRepository} from "../../../../infrastructure/user/private/local-storage-session-repository";
+import {Session} from "../../../../domain/user/session/session";
+import {ISession} from "../../../../domain/user/session/i-session";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserAccessService {
 
-  constructor(private httpUserRepository: HttpUserRepository) { }
+  constructor(
+    private httpUserRepository: HttpUserRepository,
+    private localStorageSessionRepository: LocalStorageSessionRepository,
+  ) { }
 
   registerUserByEmail(email: IUserEmail): Observable<IUserCreationResponse> {
     return this.httpUserRepository.createUserByEmail(email);
@@ -23,5 +29,13 @@ export class UserAccessService {
 
   loginUserByEmailAndPassword(email: IUserEmail, password: IUserPassword): Observable<IUserLoginResponse> {
     return this.httpUserRepository.loginUserByEmailAndPassword(email, password);
+  }
+
+  saveSession(jwt: string, createdAt: number, expiresAt: number): void {
+    this.localStorageSessionRepository.saveSession(new Session(jwt, createdAt, expiresAt));
+  }
+
+  loadSession(): ISession {
+    return this.localStorageSessionRepository.findSession();
   }
 }
